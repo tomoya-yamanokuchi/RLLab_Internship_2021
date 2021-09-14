a436c2e2212ac5f40aa99e4dc86272cd5235bd2f
tomoya-y-cluster3 <bstdcwbp@gmail.com>
2021-09-14 17:30:50 +0900

    潜在空間見えるようになった

--- MNISTDataset.py
+++ MNISTDataset.py
@@ -10,9 +10,9 @@ class MNISTDataset(Dataset):
     def load_train(self):
         (x_train, y_train), _ = keras.datasets.mnist.load_data()
         x_train = srv.format_image(x_train)
-        return x_train
+        return x_train, y_train
 
     def load_test(self):
         _, (x_test, y_test) = keras.datasets.mnist.load_data()
         x_test = srv.format_image(x_test)
-        return x_test
+        return x_test, y_test
--- VisualizeLatentSpaceUsecase.py
+++ VisualizeLatentSpaceUsecase.py
@@ -7,27 +7,46 @@ import numpy as np
 from tensorflow import keras
 from keras.callbacks import ModelCheckpoint
 
+import matplotlib
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+import matplotlib.pyplot as plt
+from matplotlib import ticker, cm
+
 
 class VisualizeLatentSpaceUsecase:
+    def get_color_and_marker(self, label):
+        cm              = plt.get_cmap('jet')
+        mnist_label_num = 10
+        colors          = []
+        markers_set     = ["*", "s", "v", "^", "+", "o", "P", "D", "p", "d"]
+        markers         = []
+        for i in list(label):
+            colors.append(cm(1.*i/mnist_label_num))
+            markers.append(markers_set[i])
+        return colors, markers, cm
+
+
     def run(self, config, model_load_path):
         # vae = keras.models.load_model("{}.h5".format(model_load_path))
 
         vae = VariationalAutoEncoder(config)
-        # vae.load_weights("{}.h5".format(model_load_path))
-        vae.compile(optimizer=keras.optimizers.Adam(
-            learning_rate = config.optimzer.lr,
-            beta_1        = config.optimzer.beta_1,
-            beta_2        = config.optimzer.beta_2,
-            epsilon       = None if config.optimzer.epsilon=='None' else config.optimzer.epsilon,
-            decay         = config.optimzer.decay,
-            amsgrad       = config.optimzer.amsgrad
-        ))
+        vae.built = True
         vae.load_weights("{}.h5".format(model_load_path))
 
-        # factory = DatasetFactory()
-        # dataset = factory.create(config.dataset.dataset_name)
-        # x_train = dataset.load_train()
+        factory = DatasetFactory()
+        dataset = factory.create(config.dataset.dataset_name)
+        x, y = dataset.load_test()
 
+        z_mean, _, _   = vae.encoder.predict(x)
+
+        colors, makers, cm = self.get_color_and_marker(y)
+        plt.figure(figsize=(12, 10))
+        plt.scatter(z_mean[:, 0], z_mean[:, 1], c=colors)
+        plt.colorbar()
+        plt.xlabel("z[0]")
+        plt.ylabel("z[1]")
+        plt.show()
 
 
 if __name__ == '__main__':
